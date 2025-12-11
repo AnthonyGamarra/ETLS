c79ca0fe446917148d3891642a0f042a8f30981c
Anthony Gamarra <agamarraj@pucp.edu.pe>
2025-12-11 13:52:25 -0500

    cambios

--- ETL_EME_DWSGE/Oracle/ETL_DW_EMERGENCIA_DEFUNCIONES_HOMOLOGACION.py
+++ ETL_EME_DWSGE/Oracle/ETL_DW_EMERGENCIA_DEFUNCIONES_HOMOLOGACION.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 import pandas as pd
+import oracledb
 import io
 from dotenv import load_dotenv
 from datetime import datetime
@@ -15,10 +16,11 @@ pg_host_src = os.getenv("PG_HOST")
 pg_db_src   = os.getenv("PG_DB")
 
 # Configuración de base de datos destino
-pg_user_dst = os.getenv("PG_USER")
-pg_pass_dst = os.getenv("PG_PASS")
-pg_host_dst = os.getenv("PG_HOST")
-pg_db_dst   = os.getenv("PG_DB")
+oracle_user2 = os.getenv("ORACLE_USER2")
+oracle_pass2 = os.getenv("ORACLE_PASS2")
+oracle_host2 = os.getenv("ORACLE_HOST2")
+oracle_port2 = os.getenv("ORACLE_PORT2")
+oracle_service2 = os.getenv("ORACLE_SERVICE2")
 
 # Crear conexión origen (para leer datos)
 conn_src = psycopg2.connect(
@@ -30,13 +32,14 @@ conn_src = psycopg2.connect(
 )
 
 # Crear conexión destino (para cargar datos)
-conn_dst = psycopg2.connect(
-    dbname=pg_db_dst,
-    user=pg_user_dst,
-    password=pg_pass_dst,
-    host=pg_host_dst,
-    port=5433
+dsn = f"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={oracle_host2})(PORT={oracle_port2}))(CONNECT_DATA=(SERVICE_NAME={oracle_service2})))"
+
+conn_dst = oracledb.connect(
+    user=oracle_user2,
+    password=oracle_pass2,
+    dsn=dsn
 )
+conn_dst.autocommit = False
 
 anio = datetime.now().year
 start_time = datetime.now()
@@ -54,22 +57,11 @@ else:
 
 print(f"\nInicio del ETL: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-table_name = "dwsge.dwe_emergencia_defunciones_homologacion"
+table_name = "DWH_SGE.dwe_emergencia_defunciones_homologacion"
 
 
-for mes in range(11,12):
+for mes in range(1,12):
     mes_str = f"{mes:02d}"
-    try:
-        cur_dst = conn_dst.cursor()
-        partition_name = f"dwsge.dwe_emergencia_defunciones_homologacion_{anio}_{mes_str}"
-        cur_dst.execute(f"TRUNCATE TABLE {partition_name};")
-        conn_dst.commit()
-        cur_dst.close()
-        print(f"Partición truncada correctamente: {partition_name}")
-    except Exception as e:
-        print(f"Error truncando partición {partition_name}: {e}")   
-    print(f"\nProcesando mes: {anio}-{mes_str}")
-    # Truncar partición del mes antes de cargar datos
 
     try:
         query = f"""
@@ -90,26 +82,44 @@ for mes in range(11,12):
 
         df = pd.read_sql_query(query, conn_src)
 
+        # -------------------------------------------
+        # Truncar partición Oracle
+        # -------------------------------------------
+        try:
+            tabla = "DWH_SGE.dwe_emergencia_defunciones_homologacion"
+            partition_name = f"P{anio}_{mes_str}"
+
+            sql_truncate = f"ALTER TABLE {tabla} TRUNCATE SUBPARTITION {partition_name}"
+
+            with conn_dst.cursor() as cur:
+                cur.execute(sql_truncate)
+                conn_dst.commit()
+
+            print(f"Partición {partition_name} truncada correctamente.")
+        except Exception as e:
+            print(f"Advertencia: error truncando partición {partition_name}: {e}")
+
         if df.empty:
             print(f"No se encontraron datos para el mes {mes_str}")
         else:
             df.columns = df.columns.str.lower()
 
-            # Exportar DataFrame a CSV en memoria
-            buffer = io.StringIO()
-            df.to_csv(buffer, index=False, header=False)
-            buffer.seek(0)
+        # ======================================================
+        # INSERTAR EN ORACLE USANDO executemany
+        # ======================================================
+                # Cargar datos
+        table_name = "DWH_SGE.dwe_emergencia_defunciones_homologacion"
+        cols = list(df.columns)
+        placeholders = ",".join([f":{i+1}" for i in range(len(cols))])
+        sql_insert = f"INSERT INTO {table_name} ({','.join(cols)}) VALUES ({placeholders})"
 
-            # Usar cursor para cargar datos con COPY
-            cur_dst = conn_dst.cursor()
-            cols = ','.join(df.columns)
-            copy_sql = f"COPY {table_name} ({cols}) FROM STDIN WITH CSV"
+        data = [tuple(row) for row in df.to_numpy()]
 
-            cur_dst.copy_expert(sql=copy_sql, file=buffer)
+        with conn_dst.cursor() as cur:
+            cur.executemany(sql_insert, data)
             conn_dst.commit()
-            cur_dst.close()
 
-            print(f"Carga con COPY completada mes {mes_str}: filas totales cargadas {len(df)}")
+        print(f"Filas cargadas en Oracle para {mes_str}: {len(df)}")
 
     except Exception as e:
         print(f"Error en mes {mes_str}: {e}")

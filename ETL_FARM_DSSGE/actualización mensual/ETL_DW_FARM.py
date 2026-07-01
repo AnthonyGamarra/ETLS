009b9f4806a97101bec38d4a50a3bfdc30159096
Anthony Gamarra <agamarraj@pucp.edu.pe>
2026-07-01 16:00:13 -0500

    cambios

--- ETL_FARM_DSSGE/actualización mensual/ETL_DW_FARM.py
+++ ETL_FARM_DSSGE/actualización mensual/ETL_DW_FARM.py
@@ -61,8 +61,8 @@ print("Conexión a PostgreSQL establecida.")
 # ==============================
 # 5. Parámetros de fechas
 # ==============================
-start_date = datetime(2019, 1, 1)
-end_date = datetime(2024, 12, 31)
+start_date = datetime(2026, 6, 1)
+end_date = datetime(2026, 6, 30)
 
 # ==============================
 # 6. Ciclo para extraer y copiar mes a mes en bloques semanales
